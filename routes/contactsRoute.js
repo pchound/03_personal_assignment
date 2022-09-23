6fb865ace24401334b30635cbfc2289569d3375f
pchound <82906754+pchound@users.noreply.github.com>
2022-09-23 16:36:16 -0600

    Beta

--- routes/contactsRoute.js
+++ routes/contactsRoute.js
@@ -1,27 +1,12 @@
+//This code will use express, instead of the outdated way
 const contacts_controller = require('../controllers/contacts_controller');
-function route(req, res)
-{
-    let method = req.method;
-    let path = req.url;
-    let uri = new URL(req.url, `http://${req.headers.host}`);
-    let params = uri.searchParams;
-    let id = params.get('id');
+const express = require('express');
+const router = express.Router();
 
-    console.log('id search results: ' + id);
-    console.log('pathname: ' + uri.pathname);
-    
-    if (method == 'GET' && uri.pathname == '/contacts' && id == null)
-    {
-        contacts_controller.contacts(req,res);
-    }
-    else if (method == 'GET' && uri.pathname == '/contacts' && id != null)
-    {
-        contacts_controller.contactsId(req,res);
-    }
-    else
-    {
-        res.writeHead(404,{'ContentType' : 'text/plain'});
-        res.end();
-    }
-}
-module.exports = route;
+//Post function
+router.post('/names',function(req, res)
+{
+    res.send({type: 'GET'});
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const dotenv = require("dotenv");
+const bodyParser = require('body-parser');
+
 dotenv.config();
 const contacts_route = require('./routes/contactsRoute.js');
 let PORT = process.env.PORT || 3000;
 const http = require('http');
+
+//app.use(bodyParser.json());
 
 let server = http.createServer((req, res) => 
 {
